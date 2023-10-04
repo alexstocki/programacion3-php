7e9f84ee61550e74c581f13cb1198614256281d6
stockia <alexstocki.dev@gmail.com>
2023-10-03 22:48:38 -0300

    init clase06

---
+++ clase06/database/AccesoDb.php
@@ -0,0 +1,32 @@
+<?php
+
+    class AccesoDb {
+        private static $ObjetoAccesoDb;
+        private $objetoPDO;
+
+        private function __construct() {
+            try {
+                $this->objetoPDO = new PDO('mysql:host=localhost;dbname=tp01;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $this->objetoPDO->exec("SET CHARACTER SET utf8");
+            } catch (PDOException $e) {
+                print "Error: " . $e->getMessage();
+                die();
+            }
+        }
+
+        public function RetornarConsulta($sql) {
+            return $this->objetoPDO->prepare($sql);
+        }
+
+        public function RetornarUltimoIdInsertado() {
+            return $this->objetoPDO-<lastInsertId();
+        }
+
+        public static function retornarObjetoAcceso() {
+            if (!isset(self::$ObjetoAccesoDb)) {
+                self::$ObjetoAccesoDb = new AccesoDb();
+            }
+            
+            return self::$ObjetoAccesoDb;
+        } 
+    }
---
+++ clase06/e27.php
@@ -0,0 +1,8 @@
+<?php
+    // Aplicación No 27 (Registro BD)
+    // Archivo: registro.php
+    // método:POST
+    // Recibe los datos del usuario( nombre,apellido, clave,mail,localidad )por POST , crear
+    // un objeto con la fecha de registro y utilizar sus métodos para poder hacer el alta,
+    // guardando los datos la base de datos
+    // retorna si se pudo agregar o no.
